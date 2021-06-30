51133be98a7ea4f3b6540c33035b8106cd120669
aslamdien <aslamdien90@gmail.com>
2021-06-30 16:34:21 +0200

    New

--- script/pokeinfo.js
+++ script/pokeinfo.js
@@ -1,7 +1,5 @@
 let base_URL = "https://pokeapi.co/api/v2/pokemon/";
 
-
-
 function getPokemonList(url) {
   fetch(url)
     // Convert data from JSON
@@ -17,13 +15,11 @@ function getPokemonList(url) {
       // Clear the container
       container.innerHTML = "";
       // Loop over pokemon list and create an HTML button for each one. Add the button to the container
-    
-    
+
       pokemon.forEach((btn) => {
-        
         container.innerHTML += `<button onclick="getPokemonInfo('${btn.url}')">${btn.name}</button>`;
       });
-        container.innerHTML += `<br><br><button class = "prev" onclick="getPokemonList('${data.previous}')">Prev</button>`
+      container.innerHTML += `<br><br><button class = "prev" onclick="getPokemonList('${data.previous}')">Prev</button>`;
       // Add a next pokemon button
       container.innerHTML += `<button class = "next" onclick="getPokemonList('${data.next}')">Next</button>`;
     });
@@ -32,12 +28,24 @@ function getPokemonList(url) {
 // Get default pokemon list
 getPokemonList(base_URL);
 
-function getPokemonInfo(url){
-  console.log(url)
+function getPokemonInfo(url) {
+  console.log(url);
   fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data)
-    document.querySelector(".pokemon-info").innerHTML = `<img src = "${data.sprites.front_default}">`
-  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      document.querySelector(
+        ".pokemon-name"
+      ).innerHTML = `<h2>${data.name}</h2>`;
+      document.querySelector(
+        ".pokemon-pic"
+      ).innerHTML = `<img src = "${data.sprites.other["official-artwork"].front_default} ">`;
+      document.querySelector(".pokemon-info").innerHTML = `<div>
+    <p>Types: <span>${data.types[0].type.name}</span></p>
+    <br>
+    <p>Abilities: <span>${data.abilities[0].ability.name}, ${data.abilities[1].ability.name}</span></p>
+    <br>
+    <p>Moves: <span>${data.moves[0].move.name}, ${data.moves[1].move.name}, ${data.moves[2].move.name}, ${data.moves[3].move.name}</span></p>
+    </div>`;
+    });
 }
